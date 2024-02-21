0bfededed15f80d571758a6e779d6f8b640fb6cf
HimanthaD4 <himanthad4@gmail.com>
2024-02-22 03:54:22 +0530

    fix issues in authentication.
    fix some issues in authentication pages. add images .
    fix issues in your_order_page.html,search.html and all other pages

--- E_shop/store_app/admin.py
+++ E_shop/store_app/admin.py
@@ -3,17 +3,11 @@ from django.contrib import admin
 # Register your models here.
 from .models import *
 
-
 class ImagesTublerinline(admin.TabularInline):
     model = Images
 
-
-class TagTublerinline(admin.TabularInline):
-    model = Tag
-
-
 class ProductAdmin(admin.ModelAdmin):
-    inlines = [ImagesTublerinline,TagTublerinline]
+    inlines = [ImagesTublerinline]
 
 
 class OrderItemTublerinline(admin.TabularInline):
@@ -26,12 +20,9 @@ class OrderAdmin(admin.ModelAdmin):
 
 
 admin.site.register(Images)
-admin.site.register(Tag)
 
 admin.site.register(Categories)
-admin.site.register(Brand)
-admin.site.register(Color)
-admin.site.register(Filter_price)
+
 admin.site.register(Product,ProductAdmin)
 admin.site.register(Contact_us)
 
