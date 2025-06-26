ab1fec7144a1ec9608c78ffaa38aaa357fb0787f
Himantha Hirushan <himanthad4@gmail.com>
2025-06-26 15:23:35 +0530

    update build.sh

--- E_shop/build.sh
+++ E_shop/build.sh
@@ -1,11 +1,18 @@
-#!/bin/sh
+#!/bin/bash
 echo "----- Starting build process -----"
 
-# Install system dependencies for PostgreSQL
-apt-get update && apt-get install -y libpq-dev python3-dev gcc
+# Install system dependencies
+apt-get update && apt-get install -y \
+    libpq-dev \
+    python3-dev \
+    gcc
 
-# Set up Python environment
-python -m pip install --upgrade pip
+# Create and activate virtual environment
+python -m venv venv
+source venv/bin/activate
+
+# Install Python dependencies
+pip install --upgrade pip
 pip install -r requirements.txt
 
 # Django setup
