f3c3fc1658a857b58a6a7bee3ad6e1a01846cfac
Himantha Hirushan <himanthad4@gmail.com>
2025-06-27 08:44:28 +0530

    update build.sh

--- E_shop/build.sh
+++ E_shop/build.sh
@@ -1,24 +1,11 @@
 #!/bin/bash
-echo "----- Starting build process -----"
-
-apt-get update && apt-get install -y \
-    libpq-dev \
-    python3-dev \
-    gcc \
-    postgresql-client
 
 python -m venv venv
 source venv/bin/activate
 
-python -m pip install --upgrade pip
-
-# You are already in the E_shop directory when running build.sh
+pip install --upgrade pip
 pip install -r requirements.txt
 
-# Collect static files from correct location
 python manage.py collectstatic --noinput
-
-# Apply migrations
+python manage.py makemigrations
 python manage.py migrate
-
-echo "----- Build completed successfully -----"
